95379bd07981173fea84d983c1f98713cf990252
Ashley Dixon <ashleydixon2012@gmail.com>
2021-01-30 16:17:17 -0700

    Added query url for the data

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+API_KEY = "5vyegmglhmbxzho7m6nglk87q"
+app_token = "39Oen6Ui90UxbFmPFmCubcIkr"
---
+++ static/js/logic.js
@@ -0,0 +1,30 @@
+var apiKey = API_KEY;
+var appToken = app_token;
+
+// Set up first chart
+var svgWidth = 950;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    bottom: 50,
+    left: 40,
+    right: 40
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// SVG wrapper to append the chart
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Transform the chart so the axis appear on the left(y-axis)/bottom(x-axis)
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Query URLs for the data 
+var queryUrl2019 = `https://opendata.fcgov.com/resource/8n27-taq6.json?$date=>=01-Jan-2019and<01-Jan-2021$$app_token=${appToken}`;
