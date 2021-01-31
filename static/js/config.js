6b367733587bbc879b199839842408ad9aaac2cf
Ashley Dixon <ashleydixon2012@gmail.com>
2021-01-30 17:55:50 -0700

    Added variables for data and created a function for a table

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,1 @@
-API_KEY = "5vyegmglhmbxzho7m6nglk87q"
 app_token = "39Oen6Ui90UxbFmPFmCubcIkr"
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,19 @@
-var apiKey = API_KEY;
 var appToken = app_token;
+
+/** Function setup to grab values
+* @param {array} rows
+* @param {integer} index
+* index 1 - Date
+* index 7 - Poudre_raw_turbidity
+* index 11 - Horsetooth_raw_turbidity
+* index 15 - Finished_water_turbidity
+*/
+
+function unpack(rows, index){
+    return rows.map(function(row) {
+        return row[index];
+    });
+}
 
 // Set up first chart
 var svgWidth = 950;
@@ -27,5 +41,17 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Query URL for the data 
-var queryUrl2019 = `https://opendata.fcgov.com/resource/8n27-taq6.json?$where=datenum%3E=20190101&$$app_token=${appToken}`;
-// console.log(queryUrl2019)
+var queryUrl = `https://opendata.fcgov.com/resource/8n27-taq6.json?$where=datenum%3E=20190101&$$app_token=${appToken}`;
+// console.log(queryUrl)
+
+// Function to extract data and put into a visible table
+function getWaterData() {
+
+    d3.json(queryUrl).then(function(data) {
+        var dates = unpack(data.dataset.data, 1);
+        var poudreturb = unpack(data.dataset.data, 7);
+        var horsetoothturb = unpack(data.dataset.data, 11);
+        var finishedturb = unpack(data.dataset.data, 15);
+        buildTable(dates, poudreturb, horsetoothturb, finishedturb);
+    })
+}
