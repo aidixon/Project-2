401360c0aa6622b65df51695699583e9bd26b0ec
Ashley Dixon <ashleydixon2012@gmail.com>
2021-02-01 22:21:55 -0700

    Table has correct iteration of dates

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,10 +8,10 @@ console.log(queryUrl)
 function getWaterData() {
     queryUrl;
     // Variables for empty lists
-var all_dates = [];
-var all_poudre = [];
-var all_horsetooth = [];
-var all_finished = [];
+    var all_dates = [];
+    var all_poudre = [];
+    var all_horsetooth = [];
+    var all_finished = [];
     response = d3.json(queryUrl, function(data) {
         console.log(data);
         for (var i = 0; i < data.length; i++) {
@@ -33,77 +33,25 @@ var all_finished = [];
     console.log(all_poudre);
     console.log(all_horsetooth);
     console.log(all_finished);
-});
 
-}
+    // Enter dates into table
+    d3.select("tbody")
+        .selectAll("tr")
+        .data(all_dates)
+        .enter()
+        .append("tr")
+        .html(function(d) {
+            return `<td>${d}</td>`;
+        })
+    
+    // Enter poudre data into table
+    // d3.select("tbody")
+    //     .selectAll("tr")
+    //     .data(all_poudre)
+    //     .enter()
+    //     .append("tr")
+    //     .html(function(d) {
+    //         return `<td>${d}</td>`;
+    //     });
+})};
 getWaterData();
-
-// Use the data to put into a table
-function buildTable() {
-    var table = d3.select("#data-table");
-    var tbody = table.select("tbody");
-    var trow;
-    for (var i = 0; i < 1; i++) {
-        trow = tbody.append("tr");
-        trow.append("td").text(all_finished);
-        trow.append("td").text(all_poudre);
-        trow.append("td").text(all_horsetooth);
-        trow.append("td").text(all_finished);
-
-    }
-}
-
-// Plotly chart
-function buildPlot() {
-    queryUrl;
-    d3.json(queryUrl, function(data) {
-        for (var i = 0; i < data.length; i++) {
-
-        // Data for the plot via the json response
-        var water_dates = data.date;
-        var poudreturb = data.poudre_turb_ntu;
-        var horsetoothturb = data.horsetooth_turb_ntu;
-        var finishedturb = data.finished_water_turb_ntu;
-
-        getWaterData();
-
-        var trace1 = {
-            type: "scatter",
-            mode: "lines",
-            x: water_dates,
-            y: finishedturb,
-            line: {
-                color: "#17BECF"
-            }
-        };
-
-        // Candlestick Trace
-        var trace2 = {
-            type: "candlestick",
-            x: water_dates,
-            poudre: poudreturb,
-            horsetooth: horsetoothturb
-        };
-
-        var data = [trace1, trace2];
-
-        var layout = {
-            title: 'Water Analysis',
-            xaxis: {
-                autorange: true,
-                type: "date",
-                title: "Dates"
-            },
-            yaxis: {
-                autorange: true,
-                type: "linear",
-                title: "Turbidity Results"
-            },
-            showlegend: true
-        };
-
-        Plotly.newPlot("plot-1", data, layout);
-    }});
-}
-
-buildPlot();
